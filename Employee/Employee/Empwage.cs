4df05b50ab2867ce7822ce96f4262580654614c0
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-10-29 17:36:46 +0530

    ADD EmployeePresentOrAbsent

---
+++ Employee/Employee/Empwage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class Empwage
+    {
+        public static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            int empRatePerHour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();//Random object has next method to gernerate random numbers
+            int empCheck = random.Next(0, 2);//generates 0 1 2
+            if (empCheck == isFullTime)
+            {
+                
+                empHrs = 8;
+            }
+            else
+            {
+                
+                empHrs = 0;
+            }
+            empWage = empRatePerHour * empHrs;
+            Console.WriteLine("Employee has earned " + empWage);
+        }
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int isFullTime = 1;
+         
+            Random random = new Random();//Random object has next method to gernerate random numbers
+            int empCheck = random.Next(0, 2);//generates 0 1 2
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+               
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+              
+            }
+          
+        }
+    }
+}
